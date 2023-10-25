fa97b4b8e6840631e8f1fb7e52239738d73b20cb
Burgess Chang <bsc@brsvh.org>
2023-10-25 17:46:52 +0800

    chore: update directory local varibales
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((prog-mode . ((eval . (display-fill-column-indicator-mode 1))))
+ (text-mode . ((eval . (auto-fill-mode 1))
+               (sentence-end-double-space . t))))
