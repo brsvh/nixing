2a4c8341fa759b2e6dd3bd8334423ff3dfd88876
Burgess Chang <bsc@brsvh.org>
2023-10-30 16:06:08 +0800

    feat: output flakeModules
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- flake-modules/default.nix
+++ flake-modules/default.nix
@@ -3,8 +3,13 @@
   imports =
     [
       ./devshell.nix
-      ./home-manager.nix
       ./pre-commit.nix
       ./treefmt.nix
     ];
+
+  flake = {
+    flakeModules = {
+      home-manager = ./home-manager;
+    };
+  };
 }
