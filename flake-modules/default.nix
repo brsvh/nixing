488159099c54aeea7918a63c1bb7f2003c62ef0d
Burgess Chang <bsc@brsvh.org>
2023-10-30 16:47:03 +0800

    fix: infinite recursion
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- flake-modules/default.nix
+++ flake-modules/default.nix
@@ -3,13 +3,14 @@
   imports =
     [
       ./devshell.nix
+      ./home-manager.nix
       ./pre-commit.nix
       ./treefmt.nix
     ];
 
   flake = {
     flakeModules = {
-      home-manager = ./home-manager;
+      home-manager = ./home-manager.nix;
     };
   };
 }
