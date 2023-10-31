80da66b5da10bd95b03dc294ea78f8d1bd786c60
Burgess Chang <bsc@brsvh.org>
2023-10-31 17:44:38 +0800

    feat(eustoma): update nix settings
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- nixos/eustoma/configuration.nix
+++ nixos/eustoma/configuration.nix
@@ -175,9 +175,41 @@ with lib;
   };
 
   nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = mkDefault ''
+        --delete-older-than 4w
+      '';
+    };
+
+    optimise = {
+      automatic = true;
+    };
+
     settings = {
-      experimental-features = [ "nix-command" "flakes" ];
-      trusted-users = [ "bsc" ];
+      allowed-users =
+        [
+          "@users"
+        ];
+
+      experimental-features =
+        [
+          "ca-derivations"
+          "flakes"
+          "nix-command"
+          "repl-flake"
+        ];
+
+      sandbox = true;
+
+      trusted-users =
+        [
+          "@admin"
+          "@wheel"
+          "root"
+        ];
+
       use-xdg-base-directories = true;
     };
   };
