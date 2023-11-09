ed85cfa2922fe1b60cdb24568b77fbbd97e26678
Burgess Chang <bsc@brsvh.org>
2023-11-09 15:27:11 +0800

    chore: update eustoma host key
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- flakes/nixago.nix
+++ flakes/nixago.nix
@@ -27,6 +27,9 @@
                       age = "age1h8jgr473q6vj9e8kannr0ljzreu7whc46qhjfpjxxkl4w38ny5esz6mk0v";
                       pgp = "7B740DB9F2AC6D3B226BC53078D74502D92E0218";
                     };
+                    eustoma = {
+                      age = "age1lgy77wf7vxlvvv8lzsgmq6wgf43c4hl93ls2mw8pspmdcuzqvems7svu6t";
+                    };
                   in
                   {
                     creation_rules =
@@ -37,6 +40,14 @@
                             {
                               pgp = [ bsc.pgp ];
                               age = [ bsc.age ];
+                            }
+                          ];
+                        }
+                        {
+                          path_regex = "^secrets/eustoma\.yaml$";
+                          key_groups = [
+                            {
+                              age = [ eustoma.age ];
                             }
                           ];
                         }
