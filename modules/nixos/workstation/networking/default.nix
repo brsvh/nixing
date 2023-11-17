310b631b1f4f810e6242223989dfa0a893196d78
Burgess Chang <bsc@brsvh.org>
2023-11-17 10:55:44 +0800

    feat: add workstation.networking.network-manager
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- modules/nixos/workstation/networking/default.nix
+++ modules/nixos/workstation/networking/default.nix
@@ -1,7 +1,34 @@
-{ ... }:
+{ config
+, lib
+, pkgs
+, ...
+}:
+with lib;
+let
+  cfg = config.workstation.networking;
+in
 {
   imports =
     [
       ./proxy
+
+      ./network-manager.nix
     ];
+
+  options = {
+    workstation = {
+      networking = {
+        manager = mkOption {
+          type = types.enum
+            [
+              "network-manager"
+            ];
+          default = "network-manager";
+          description = ''
+            The networking manage backend.
+          '';
+        };
+      };
+    };
+  };
 }
---
+++ modules/nixos/workstation/networking/network-manager.nix
@@ -0,0 +1,26 @@
+{ config
+, lib
+, pkgs
+, ...
+}:
+with lib;
+let
+  cfg = config.workstation.networking;
+
+  withNetworkManager = cfg.manager == "network-manager";
+in
+{
+  config = mkMerge
+    [
+      (
+        mkIf withNetworkManager
+          {
+            networking = {
+              networkmanager = {
+                enable = true;
+              };
+            };
+          }
+      )
+    ];
+}
