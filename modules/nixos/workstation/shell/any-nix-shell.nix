3e81ce8c3a54eed285e57ddf844c99187b02c0d8
Burgess Chang <bsc@brsvh.org>
2023-12-26 11:24:09 +0800

    feat: add workstation.shell.{fish,any-nix-shell}
    Allows switching of system shells via `config.workstation.shell.fish`
    related settings. Additional starship and any-nix-shell are also
    supported.
    
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

---
+++ modules/nixos/workstation/shell/any-nix-shell.nix
@@ -0,0 +1,55 @@
+{ config
+, lib
+, pkgs
+, ...
+}:
+with lib;
+let
+  cfg = config.programs.any-nix-shell;
+
+  initOption =
+    if cfg.interactiveOnly then
+      "interactiveShellInit"
+    else
+      "shellInit";
+in
+{
+  options = {
+    programs = {
+      any-nix-shell = {
+        enable = mkOption {
+          type = types.bool;
+          default = false;
+          description = ''
+            Whether use any-nix-shell.
+          '';
+        };
+
+        interactiveOnly = mkOption {
+          default = true;
+          example = false;
+          type = types.bool;
+          description = ''
+            Whether to enable any-nix-shell only in interactive shell.
+          '';
+        };
+      };
+    };
+  };
+
+  config = mkIf cfg.enable
+    {
+      environment = {
+        systemPackages =
+          [
+            pkgs.any-nix-shell
+          ];
+      };
+
+      programs = {
+        fish.${initOption} = ''
+          any-nix-shell fish --info-right | source
+        '';
+      };
+    };
+}
