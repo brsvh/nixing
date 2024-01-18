3c177da4c2fad1be7c15dc4b7ddbeb58c788b771
Burgess Chang <bsc@brsvh.org>
2024-01-18 17:31:12 +0800

    chore: bump version
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- flake.nix
+++ flake.nix
@@ -9,11 +9,26 @@
         "nix-command"
         "repl-flake"
       ];
+
+    substituters =
+      [
+        "https://cache.nixos.org"
+      ];
+
+    extra-substituters =
+      [
+        "https://nix-community.cachix.org"
+      ];
+
+    extra-trusted-public-keys =
+      [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
   };
 
   inputs = {
     brsvh-emacs = {
-      url = "github:brsvh/emacs.d/develop";
+      url = "github:brsvh/emacs.d/main";
       inputs = {
         devshell = {
           follows = "devshell";
@@ -198,7 +213,7 @@
       follows = "nixpkgs-stable";
     };
     nixpkgs-stable = {
-      url = "github:NixOS/nixpkgs/nixos-23.05";
+      url = "github:NixOS/nixpkgs/nixos-23.11";
     };
     nixpkgs-unstable = {
       url = "github:NixOS/nixpkgs/nixos-unstable";
