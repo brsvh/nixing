dde61db54573a950b5aa1a6eea346150dc9c6ebc
Burgess Chang <bsc@brsvh.org>
2024-01-29 09:44:51 +0800

    feat: create iosevka-{source-code-pro,plex} packages
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

---
+++ pkgs/iosevka-source-code-pro.nix
@@ -0,0 +1,29 @@
+{ iosevka
+, ...
+}:
+iosevka.override {
+  set = "source-code-pro";
+
+  privateBuildPlan = ''
+    [buildPlans.iosevka-source-code-pro]
+    family = "Iosevka Source Code Pro"
+    spacing = "fontconfig-mono"
+    serifs = "sans"
+    noCvSs = true
+    exportGlyphNames = true
+    noLigation = true
+
+    [buildPlans.iosevka-source-code-pro.variants]
+    inherits = "ss09"
+
+    [buildPlans.iosevka-source-code-pro.variantswidths.Condensed]
+    shape = 500
+    menu = 3
+    css = "condensed"
+    
+    [buildPlans.iosevka-source-code-pro.variants.widths.Normal]
+    shape = 600
+    menu = 5
+    css = "normal"
+  '';
+}
