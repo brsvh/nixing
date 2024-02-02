963eae20ddb913056c506cf187918b1c3efef918
Burgess Chang <bsc@brsvh.org>
2024-02-02 13:45:57 +0800

    feat(bsc): add GitHub and GitLab search engines
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- home/bsc/browser.nix
+++ home/bsc/browser.nix
@@ -48,38 +48,186 @@ in
               color = "purple";
               id = 4;
             };
+
+            "Others" = {
+              icon = "fence";
+              color = "toolbar";
+              id = 5;
+            };
           };
 
           search = {
             default = "Google";
 
             engines = {
-              "Nix Packages" = {
+              "GitHub" = {
+                definedAliases = [ "@github" ];
+
                 urls =
                   [
                     {
-                      template = "https://search.nixos.org/packages";
-                      params = [
-                        { name = "type"; value = "packages"; }
-                        { name = "query"; value = "{searchTerms}"; }
-                      ];
+                      template = "https://github.com/search";
+                      parmas =
+                        [
+                          {
+                            name = "q";
+                            value = "{searchTerms}";
+                          }
+                        ];
                     }
                   ];
 
-                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                definedAliases = [ "@nixpkgs" ];
+                iconUpdateURL = "https://github.com/favicon.ico";
+                updateInterval = 24 * 60 * 60 * 1000;
+              };
+
+              "GitHub Code" = {
+                definedAliases = [ "@github-code" ];
+
+                urls =
+                  [
+                    {
+                      template = "https://github.com/search";
+                      parmas =
+                        [
+                          {
+                            name = "q";
+                            value = "{searchTerms}";
+                          }
+                          {
+                            name = "type";
+                            value = "code";
+                          }
+                        ];
+                    }
+                  ];
+
+                iconUpdateURL = "https://github.com/favicon.ico";
+                updateInterval = 24 * 60 * 60 * 1000;
+              };
+
+              "GitHub Repositories" = {
+                definedAliases = [ "@github-repo" ];
+
+                urls =
+                  [
+                    {
+                      template = "https://github.com/search";
+                      parmas =
+                        [
+                          {
+                            name = "q";
+                            value = "{searchTerms}";
+                          }
+                          {
+                            name = "type";
+                            value = "repositories";
+                          }
+                        ];
+                    }
+                  ];
+
+                iconUpdateURL = "https://github.com/favicon.ico";
+                updateInterval = 24 * 60 * 60 * 1000;
+              };
+
+              "GitLab" = {
+                definedAliases = [ "@gitlab" ];
+
+                urls =
+                  [
+                    {
+                      template = "https://gitlab.com/search";
+                      parmas =
+                        [
+                          {
+                            name = "search";
+                            value = "{searchTerms}";
+                          }
+                        ];
+                    }
+                  ];
+
+                iconUpdateURL = "https://gitlab.com/favicon.ico";
+                updateInterval = 24 * 60 * 60 * 1000;
+              };
+
+              "GitLab Projects" = {
+                definedAliases = [ "@gitlab-projects" ];
+
+                urls =
+                  [
+                    {
+                      template = "https://gitlab.com/search";
+                      parmas =
+                        [
+                          {
+                            name = "search";
+                            value = "{searchTerms}";
+                          }
+                          {
+                            name = "scope";
+                            value = "projects";
+                          }
+                        ];
+                    }
+                  ];
+
+                iconUpdateURL = "https://gitlab.com/favicon.ico";
+                updateInterval = 24 * 60 * 60 * 1000;
               };
 
               "NixOS Options" = {
                 urls =
                   [
                     {
-                      template = "https://search.nixos.org/options?type=options&channel=unstable&query={searchTerms}";
+                      template = "https://search.nixos.org/packages";
+                      params = [
+                        {
+                          name = "type";
+                          value = "options";
+                        }
+                        {
+                          name = "channel";
+                          value = "unstable";
+                        }
+                        {
+                          name = "query";
+                          value = "{searchTerms}";
+                        }
+                      ];
                     }
                   ];
 
                 icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
                 definedAliases = [ "@nixopts" ];
+              };
+
+              "Nix Packages" = {
+                definedAliases = [ "@nixpkgs" ];
+
+                urls =
+                  [
+                    {
+                      template = "https://search.nixos.org/packages";
+                      params = [
+                        {
+                          name = "type";
+                          value = "packages";
+                        }
+                        {
+                          name = "channel";
+                          value = "unstable";
+                        }
+                        {
+                          name = "query";
+                          value = "{searchTerms}";
+                        }
+                      ];
+                    }
+                  ];
+
+                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
               };
 
               "Amazon".metaData.hidden = true;
