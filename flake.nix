668c72991467449640bc097bba774e5b36e5eeec
Burgess Chang <bsc@brsvh.org>
2024-05-19 23:56:51 +0800

    feat: add substituters
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- flake.nix
+++ flake.nix
@@ -8,6 +8,23 @@
       "flakes"
       "nix-command"
     ];
+
+    extra-substituters = [
+      "https://brsvh.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+
+    extra-trusted-public-keys = [
+      "brsvh.cachix.org-1:DqtlvqnpP9g39l8Eo74AXRftGx1KJLid/ViADTNgDNE="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+
+    substituters = [
+      "https://mirrors.cernet.edu.cn/nix-channels/store"
+      "https://mirror.nju.edu.cn/nix-channels/store"
+      "https://mirror.sjtu.edu.cn/nix-channels/store"
+      "https://cache.nixos.org"
+    ];
   };
 
   # Home Manager
