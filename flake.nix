2517e56b077d3923826c3b68149c144c55b5d2cb
Burgess Chang <bsc@brsvh.org>
2024-05-19 10:07:51 +0800

    feat: add flake-parts to make flake
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- flake.nix
+++ flake.nix
@@ -10,13 +10,57 @@
     ];
   };
 
-  # Nix flake tools
+  # Nix packages
+  inputs = {
+    nixpkgs = {
+      follows = "nixpkgs-unstable";
+    };
+    nixpkgs-stable = {
+      url = "github:NixOS/nixpkgs/nixos-23.11";
+    };
+    nixpkgs-unstable = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+  };
+
+  # NixOS packages
+  inputs = {
+    nixos = {
+      follows = "nixos-unstable";
+    };
+    nixos-stable = {
+      url = "github:NixOS/nixpkgs/nixos-23.11";
+    };
+    nixos-unstable = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+  };
+
+  # Nix libraries
   inputs = {
     flake-compat = {
       url = "github:edolstra/flake-compat/master";
       flake = false;
     };
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts/main";
+      inputs = {
+        nixpkgs-lib = {
+          follows = "nixpkgs";
+        };
+      };
+    };
+    nix-systems = {
+      url = "github:nix-systems/x86_64-linux/main";
+    };
   };
 
-  outputs = _: { };
+  outputs =
+    inputs@{ flake-parts, nix-systems, ... }:
+    let
+      inherit (flake-parts.lib) mkFlake;
+
+      systems = import nix-systems;
+    in
+    mkFlake { inherit inputs; } { inherit systems; };
 }
