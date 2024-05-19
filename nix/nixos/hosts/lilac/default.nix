6ecc12f9ed23b5ce81820bde03b1d8d579474721
Burgess Chang <bsc@brsvh.org>
2024-05-20 03:15:06 +0800

    feat: set the size of swapfile to 16G
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- nix/nixos/hosts/lilac/default.nix
+++ nix/nixos/hosts/lilac/default.nix
@@ -256,7 +256,12 @@ in
     };
   };
 
-  swapDevices = [ { device = "/var/lib/swapfile"; } ];
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 16 * 1024;
+    }
+  ];
 
   system = {
     stateVersion = "24.05";
