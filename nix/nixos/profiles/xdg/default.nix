d3b15d787e96bb4669ed1acbed9c374300be3c61
Burgess Chang <bsc@brsvh.org>
2024-05-20 17:59:09 +0800

    feat: ensure global XDG Base Directory
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- nix/nixos/profiles/xdg/default.nix
+++ nix/nixos/profiles/xdg/default.nix
@@ -1,4 +1,15 @@
 {
+  environment = {
+    sessionVariables = {
+      XDG_CACHE_HOME = "$HOME/.cache";
+      XDG_CONFIG_HOME = "$HOME/.config";
+      XDG_DATA_HOME = "$HOME/.local/share";
+      XDG_STATE_HOME = "$HOME/.local/state";
+
+      GNUPGHOME = "$XDG_STATE_HOME/gnupg";
+    };
+  };
+
   xdg = {
     portal = {
       enable = true;
