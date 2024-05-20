fd2580e5582209096fe9a5f304f3131a45771408
Burgess Chang <bsc@brsvh.org>
2024-05-20 17:51:10 +0800

    feat: add bsc@eustoma key
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- nix/nixos/users/changbingshan/default.nix
+++ nix/nixos/users/changbingshan/default.nix
@@ -50,6 +50,15 @@ in
 
         initialHashedPassword = "$6$cB3EK3Lynl./0Bio$bgH7P93D1lpgvEIJ3iks7Dk2IyNue7ria2aH8.xkZZ1PPooxkb7p/bEMN1UtJaV0TIeVr/eTY8oAN38vBgMKe0";
         isNormalUser = true;
+
+        openssh = {
+          authorizedKeys = {
+            keys = [
+              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF8kaNm6jre8kIxo3LSWe9Ohz8tXc4jRcSCw1BeoC7tj bsc@eustoma"
+            ];
+          };
+        };
+
         shell = pkgs.fish;
       };
     };
