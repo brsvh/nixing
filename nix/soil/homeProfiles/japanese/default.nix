d13162ddda380e2cc10b51e217197f734860ecc9
Burgess Chang <bsc@brsvh.org>
2024-05-22 06:48:39 +0800

    feat: remove glibcLocales
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- nix/soil/homeProfiles/japanese/default.nix
+++ nix/soil/homeProfiles/japanese/default.nix
@@ -1,11 +1,1 @@
-{
-  i18n = {
-    glibcLocales = [
-      # EUC
-      "ja_JP.EUC-JP/EUC-JP"
-
-      # UTF-8
-      "ja_JP.UTF-8/UTF-8"
-    ];
-  };
-}
+{ i18n = { }; }
--- nix/soil/homeProfiles/korean/default.nix
+++ nix/soil/homeProfiles/korean/default.nix
@@ -1,11 +1,1 @@
-{
-  i18n = {
-    glibcLocales = [
-      # EUC
-      "ko_KR.EUC-KR/EUC-KR"
-
-      # UTF-8
-      "ko_KR.UTF-8/UTF-8"
-    ];
-  };
-}
+{ i18n = { }; }
