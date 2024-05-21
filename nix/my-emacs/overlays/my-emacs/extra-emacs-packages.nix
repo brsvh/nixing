61a9be501dfcfc3ebfc61259ac216e7b94837f13
Burgess Chang <bsc@brsvh.org>
2024-05-22 04:47:45 +0800

    chore: remove license header
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- nix/my-emacs/overlays/my-emacs/extra-emacs-packages.nix
+++ nix/my-emacs/overlays/my-emacs/extra-emacs-packages.nix
@@ -1,20 +1,3 @@
-# Copyright (C) 2022-2024 Burgess Chang
-
-# This file is part of my-emacs.
-
-# my-emacs is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the
-# Free Software Foundation, either version 3 of the License, or (at your
-# option) any later version.
-
-# my-emacs is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with my-emacs.  If not, see <https://www.gnu.org/licenses/>.
-
 # NOTE: All packages related to magit should use the versions available
 #       in MELPA.
 epkgs:
