3a925624c10294a4acbbdf8e3066eeba95cc8881
Burgess Chang <bsc@brsvh.org>
2024-05-22 04:16:59 +0800

    feat: initial rtkit profile
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- nix/soil/nixosSuites/laptop/default.nix
+++ nix/soil/nixosSuites/laptop/default.nix
@@ -8,6 +8,7 @@
     cell.nixosProfiles.network-manager
     cell.nixosProfiles.pipewire
     cell.nixosProfiles.plymouth
+    cell.nixosProfiles.rtkit
     cell.nixosProfiles.systemd-boot
     cell.nixosProfiles.touchpad
     cell.nixosProfiles.zram
