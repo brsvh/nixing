69179a18daac04b7afed626b17145e07331236eb
Burgess Chang <bsc@brsvh.org>
2024-05-22 18:23:08 +0800

    feat: disable unused fish integration
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- nix/soil/homeProfiles/direnv/default.nix
+++ nix/soil/homeProfiles/direnv/default.nix
@@ -4,7 +4,6 @@
     direnv = {
       enable = true;
       enableBashIntegration = config.programs.bash.enable;
-      enableFishIntegration = config.programs.fish.enable;
 
       nix-direnv = {
         enable = true;
