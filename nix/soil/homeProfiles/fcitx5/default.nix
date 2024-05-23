94d49492d2601e979bbbb1926b278065cda57102
Burgess Chang <bsc@brsvh.org>
2024-05-23 14:58:33 +0800

    feat: generate ini and yaml from nix
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

---
+++ nix/soil/homeProfiles/fcitx5/default.nix
@@ -0,0 +1,128 @@
+{
+  cell,
+  config,
+  inputs,
+  pkgs,
+  ...
+}:
+let
+  inherit (inputs.lib) generators;
+in
+{
+  i18n = {
+    inputMethod = {
+      enabled = "fcitx5";
+    };
+  };
+
+  xdg = {
+    configFile = {
+      "fcitx5/config".source = pkgs.writeText "fcitx5_config.conf" (
+        generators.toINI {
+          Hotkey = {
+            EnumerateWithTriggerKeys = true;
+            EnumerateForwardKeys = null;
+            EnumerateBackwardKeys = null;
+            EnumerateSkipFirst = null;
+          };
+
+          "Hotkey/TriggerKeys" = {
+            "0" = "Super+space";
+            "1" = "Zenkaku_Hankaku";
+            "2" = "Hangul";
+          };
+
+          "Hotkey/AltTriggerKeys" = {
+            "0" = "Shift_L";
+          };
+
+          "Hotkey/ActivateKeys" = {
+            "0" = "Hangul_Hanja";
+          };
+
+          "Hotkey/DeactivateKeys" = {
+            "0" = "Hangul_Romaja";
+          };
+
+          "Hotkey/PrevPage" = {
+            "0" = "Up";
+          };
+
+          "Hotkey/NextPage" = {
+            "0" = "Down";
+          };
+
+          "Hotkey/PrevCandidate" = {
+            "0" = "Shift+Tab";
+          };
+
+          "Hotkey/NextCandidate" = {
+            "0" = "Tab";
+          };
+
+          "Hotkey/TogglePreedit" = {
+            "0" = "Control+Alt+P";
+          };
+
+          Behavior = {
+            AllowInputMethodForPassword = false;
+            AutoSavePeriod = "30";
+            CompactInputmethodInformation = true;
+            CustomXkbOption = null;
+            DefaultPageSize = "8";
+            DisabledAddons = null;
+            EnabledAddons = null;
+            OverriedXkbOption = false;
+            PreeditEnabledByDefault = true;
+            PreloadInputMethod = true;
+            ShareInputState = "No";
+            ShowFirstInputMethodInformation = true;
+            ShowInputMethodInformation = true;
+            ShowInputMethodInformationWhenFocusIn = false;
+            ShowPreeditForPassword = false;
+            ActiveByDefault = true;
+          };
+        }
+      );
+
+      "fcitx5/profile".source = pkgs.writeText "fcitx5_profie.conf" (
+        generators.toINI { } {
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "rime";
+          };
+
+          "Groups/0/Items/0" = {
+            Name = "rime";
+            Layout = null;
+          };
+
+          "GroupOrder" = {
+            "0" = "Default";
+          };
+        }
+      );
+
+      "fcitx5/conf/classicui.conf".source = pkgs.writeText "fcitx5_classicui.conf" (
+        generators.toINIWithGlobalSection { } {
+          globalSection = {
+            "Vertical Candidate List" = false;
+            DarkTheme = "default-dark";
+            Font = "${config.font.fontconfig.chinese.defaultFont.serif} 11";
+            MenuFont = "${config.font.fontconfig.chinese.defaultFont.serif} 11";
+            PerScreenDPI = true;
+            PreferTextIcon = false;
+            ShowLayoutNameInIcon = true;
+            Theme = "default";
+            TrayFont = "${config.font.fontconfig.chinese.defaultFont.sansSerif} 11";
+            UseAccentColor = true;
+            UseDarkTheme = false;
+            UseInputMethodLanguageToDisplayText = true;
+            WheelForPaging = true;
+          };
+        }
+      );
+    };
+  };
+}
--- nix/soil/homeProfiles/rime/default.nix
+++ nix/soil/homeProfiles/rime/default.nix
@@ -1,219 +1,12 @@
+{ pkgs, ... }:
 let
-  fcitx5Config = ''
-    [Hotkey]
-    EnumerateWithTriggerKeys=True
-    EnumerateForwardKeys=
-    EnumerateBackwardKeys=
-    EnumerateSkipFirst=False
-    EnumerateGroupForwardKeys=
-    EnumerateGroupBackwardKeys=
-
-    [Hotkey/TriggerKeys]
-    0=Super+space
-    1=Zenkaku_Hankaku
-    2=Hangul
-
-    [Hotkey/AltTriggerKeys]
-    0=Shift_L
-
-    [Hotkey/ActivateKeys]
-    0=Hangul_Hanja
-
-    [Hotkey/DeactivateKeys]
-    0=Hangul_Romaja
-
-    [Hotkey/PrevPage]
-    0=Up
-
-    [Hotkey/NextPage]
-    0=Down
-
-    [Hotkey/PrevCandidate]
-    0=Shift+Tab
-
-    [Hotkey/NextCandidate]
-    0=Tab
-
-    [Hotkey/TogglePreedit]
-    0=Control+Alt+P
-
-    [Behavior]
-    ActiveByDefault=True
-    ShareInputState=No
-    PreeditEnabledByDefault=True
-    ShowInputMethodInformation=True
-    showInputMethodInformationWhenFocusIn=False
-    CompactInputMethodInformation=True
-    ShowFirstInputMethodInformation=True
-    DefaultPageSize=8
-    OverrideXkbOption=False
-    CustomXkbOption=
-    EnabledAddons=
-    DisabledAddons=
-    PreloadInputMethod=True
-    AllowInputMethodForPassword=False
-    ShowPreeditForPassword=False
-    AutoSavePeriod=30
-  '';
-
-  fcitx5Profile = ''
-    [Groups/0]
-    Name=Default
-    Default Layout=us
-    DefaultIM=rime
-
-    [Groups/0/Items/0]
-    Name=rime
-    Layout=
-
-    [GroupOrder]
-    0=Default
-  '';
-
-  lunaPinyinSimpConfig = ''
-    patch:punctuator/half_shape:
-      "!": "！"
-      '"':
-        pair:
-          - "“"
-          - "”"
-      "#": "#"
-      "$":
-        - "￥"
-        - "$"
-        - "€"
-        - "～"
-        - "×"
-        - "÷"
-        - "°"
-        - "℃"
-        - "‰"
-        - "‱"
-        - "℉"
-        - "©"
-        - "®"
-      "%": "%"
-      "&": "&"
-      "'":
-        pair:
-          - "‘"
-          - "’"
-      "*": "*"
-      "+": "+"
-      ",": "，"
-      "-": "-"
-      ".": "。"
-      "/": "/"
-      "\\": "、"
-      ":": "："
-      ";": "；"
-      "=": "="
-      "?": "？"
-      "@": "@"
-      "(": "（"
-      ")": "）"
-      "[": "【"
-      "]": "】"
-      "{": "「"
-      "}": "」"
-      "<": "《"
-      ">": "》"
-      "^": "……"
-      "_": "——"
-      "`": "`"
-      "|": "·"
-      "~": "~"
-    recognizer/patterns/reverse_lookup:
-
-  '';
-
-  rimeCustomConfig = ''
-    patch:
-      schema_list:
-        - schema: "luna_pinyin_simp"
-      ascii_composer/good_old_caps_lock: true
-      ascii_composer/switch_key/Caps_Lock: "commit_text"
-      ascii_composer/switch_key/Shift_L: "commit_code"
-      ascii_composer/switch_key/Shift_R: "commit_code"
-
-      switcher/caption: "【设置菜单】"
-      switcher/option_list_separator: "/"
-      switcher/hotkeys:
-        - "F4"
-      menu/page_size: 8
-      key_binder/bindings:
-        - accept: "Control+p"
-          send: "Up"
-          when: "composing"
-        - accept: "Control+n"
-          send: "Down"
-          when: "composing"
-        - accept: "Control+b"
-          send: "Left"
-          when: "composing"
-        - accept: "Control+f"
-          send: "Right"
-          when: "composing"
-        - accept: "Alt+b"
-          send: "Shift+Left"
-          when: "composing"
-        - accept: "Alt+f"
-          send: "Shift+Right"
-          when: "composing"
-        - accept: "Control+a"
-          send: "Home"
-          when: "composing"
-        - accept: "Control+e"
-          send: "End"
-          when: "composing"
-        - accept: "Control+d"
-          send: "Delete"
-          when: "composing"
-        - accept: "Control+h"
-          send: "BackSpace"
-          when: "composing"
-        - accept: "Alt+h"
-          send: "Shift+BackSpace"
-          when: "composing"
-        - accept: "Control+g"
-          send: "Escape"
-          when: "composing"
-        - accept: "Control+bracketleft"
-          send: "Escape"
-          when: "composing"
-        - accept: "Alt+v"
-          send: "Page_Up"
-          when: "composing"
-        - accept: "Control+v"
-          send: "Page_Down"
-          when: "composing"
-        - accept: "Tab"
-          send: "Down"
-          when: "has_menu"
-        - accept: "ISO_Left_Tab"
-          send: "Up"
-          when: "has_menu"
-        - accept: "Shift+Tab"
-          send: "Up"
-          when: "has_menu"
-        - accept: "minus"
-          send: "Page_Up"
-          when: "has_menu"
-        - accept: "equal"
-          send: "Page_Down"
-          when: "has_menu"
-        - accept: "bracketleft"
-          send: "Page_Up"
-          when: "has_menu"
-        - accept: "bracketright"
-          send: "Page_Down"
-          when: "has_menu"
-  '';
+  genJSON = name: value: (pkgs.formats.json { }).generate name value;
 in
 {
+  imports = [ cell.homeProfiles.fcitx5 ];
+
   i18n = {
     inputMethod = {
-      enabled = "fcitx5";
       fcitx5 = {
         addons = with pkgs; [ fcitx5-rime ];
       };
@@ -222,10 +15,204 @@ in
 
   xdg = {
     dataFile = {
-      "fcitx5/config".text = fcitx5Config;
-      "fcitx5/profile".text = fcitx5Profile;
-      "fcitx5/rime/default.custom.yaml".text = rimeCustomConfig;
-      "fcitx5/rime/luna_pinyin_simp.custom.yaml".text = lunaPinyinSimpConfig;
+      "fcitx5/rime/default.custom.yaml".source = genJSON "rime_default.custom.yaml" {
+        patch = {
+          "punctuator/half_shape" = {
+            "!" = "！";
+
+            "\"" = {
+              pair = [
+                "“"
+                "”"
+              ];
+            };
+
+            "#" = "#";
+
+            "$" = [
+              "￥"
+              "$"
+              "€"
+              "～"
+              "×"
+              "÷"
+              "°"
+              "℃"
+              "‰"
+              "‱"
+              "℉"
+              "©"
+              "®"
+            ];
+
+            "%" = "%";
+            "&" = "&";
+
+            "'" = {
+              pair = [
+                "‘"
+                "’"
+              ];
+            };
+
+            "*" = "*";
+            "+" = "+";
+            "," = "，";
+            "-" = "-";
+            "." = "。";
+            "/" = "/";
+            "\\" = "、";
+            ":" = "：";
+            ";" = "；";
+            "=" = "=";
+            "?" = "？";
+            "@" = "@";
+            "(" = "（";
+            ")" = "）";
+            "[" = "【";
+            "]" = "】";
+            "{" = "「";
+            "}" = "」";
+            "<" = "《";
+            ">" = "》";
+            "^" = "……";
+            "_" = "——";
+            "`" = "`";
+            "|" = "·";
+            "~" = "~";
+          };
+        };
+      };
+      "fcitx5/rime/luna_pinyin_simp.custom.yaml".source = genJSON "rime_luna_pinyin_simp.custom.yaml" {
+        patch = {
+          schema_list = [ { schema = "luna_pinyin_simp"; } ];
+
+          "ascii_composer/good_old_caps_lock" = true;
+          "ascii_composer/switch_key/Caps_Lock" = "commit_text";
+          "ascii_composer/switch_key/Shift_L" = "commit_code";
+          "ascii_composer/switch_key/Shift_R" = "commit_code";
+
+          "switcher/caption" = "【设置菜单】";
+          "switcher/option_list_separator" = "/";
+
+          "switcher/hotkeys" = [ "F4" ];
+
+          "menu/page_size" = 8;
+
+          "key_binder/bindings" = [
+            {
+              accept = "Control+p";
+              send = "Up";
+              when = "composing";
+            }
+            {
+              accept = "Control+n";
+              send = "Down";
+              when = "composing";
+            }
+            {
+              accept = "Control+b";
+              send = "Left";
+              when = "composing";
+            }
+            {
+              accept = "Control+f";
+              send = "Right";
+              when = "composing";
+            }
+            {
+              accept = "Alt+b";
+              send = "Shift+Left";
+              when = "composing";
+            }
+            {
+              accept = "Alt+f";
+              send = "Shift+Right";
+              when = "composing";
+            }
+            {
+              accept = "Control+a";
+              send = "Home";
+              when = "composing";
+            }
+            {
+              accept = "Control+e";
+              send = "End";
+              when = "composing";
+            }
+            {
+              accept = "Control+d";
+              send = "Delete";
+              when = "composing";
+            }
+            {
+              accept = "Control+h";
+              send = "BackSpace";
+              when = "composing";
+            }
+            {
+              accept = "Alt+h";
+              send = "Shift+BackSpace";
+              when = "composing";
+            }
+            {
+              accept = "Control+g";
+              send = "Escape";
+              when = "composing";
+            }
+            {
+              accept = "Control+bracketleft";
+              send = "Escape";
+              when = "composing";
+            }
+            {
+              accept = "Alt+v";
+              send = "Page_Up";
+              when = "composing";
+            }
+            {
+              accept = "Control+v";
+              send = "Page_Down";
+              when = "composing";
+            }
+            {
+              accept = "Tab";
+              send = "Down";
+              when = "has_menu";
+            }
+            {
+              accept = "ISO_Left_Tab";
+              send = "Up";
+              when = "has_menu";
+            }
+            {
+              accept = "Shift+Tab";
+              send = "Up";
+              when = "has_menu";
+            }
+            {
+              accept = "minus";
+              send = "Page_Up";
+              when = "has_menu";
+            }
+            {
+              accept = "equal";
+              send = "Page_Down";
+              when = "has_menu";
+            }
+            {
+              accept = "bracketleft";
+              send = "Page_Up";
+              when = "has_menu";
+            }
+            {
+              accept = "bracketright";
+              send = "Page_Down";
+              when = "has_menu";
+            }
+          ];
+        };
+      };
     };
   };
 }
