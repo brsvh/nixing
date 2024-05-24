0377abc40d728cc5bed3daca168ae81da90d25af
Burgess Chang <bsc@brsvh.org>
2024-05-24 22:21:34 +0800

    feat: initial fonts.{homeModules,nixosModules}.tsangertype-fonts
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

---
+++ nix/fonts/homeModules/tsangertype-fonts/default.nix
@@ -0,0 +1,75 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.fonts.tsangertype-fonts;
+in
+{
+  options.fonts.tsangertype-fonts = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = lib.mdDoc ''
+        Whether use TsangerType fonts.
+      '';
+    };
+
+    excludeFonts = lib.mkOption {
+      type = with lib.types; listOf package;
+      default = with pkgs.tsangertypeFonts; [ ];
+      description = lib.mdDoc ''
+        Fonts will be excluded from config.fonts.tsangertype-fonts.fonts.
+      '';
+    };
+
+    fonts = lib.mkOption {
+      type = with lib.types; listOf package;
+      default = with pkgs.tsangertypeFonts; listOfGratisProPersonaFonts tsangertypeFonts;
+      description = lib.mdDoc ''
+        Available fonts.
+
+        For example, filters out all fonts in a family (jinkai here).
+
+        ```nix
+        with lib;
+        with pkgs.tsangertypeFonts;
+        let
+          pname = drv: attrByPath [ "pname" ] "unknown" drv;
+          cond = drv: hasPrefix "tsangertype-jinkai" (pname drv);
+        in
+        (listOfFontsWithCond gratisProPersona cond)
+        ```
+
+        Or simply combine multiple specified fonts.
+
+        ```nix
+        with pkgs.tsangertypeFonts;
+        [
+          tsangertype-jinkai-01-w01-font
+          tsangertype-jinkai-01-w02-font
+          # ...
+        ]
+        ```
+      '';
+    };
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      description = lib.mdDoc ''
+        Fonts will be  installed.
+      '';
+      default = pkgs.tsangertypeFonts.combine (
+        with builtins; filter (font: !(elem font cfg.excludedFonts)) cfg.fonts
+      );
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home = {
+      packages = [ cfg.package ];
+    };
+  };
+}
