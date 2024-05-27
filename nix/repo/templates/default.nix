f02e3cabd88b2748380ba85cd8df735c14b78757
Burgess Chang <bsc@brsvh.org>
2024-05-27 13:22:10 +0800

    feat: add flake-utils template
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- nix/repo/templates/default.nix
+++ nix/repo/templates/default.nix
@@ -1,7 +1,12 @@
 { cell, inputs }:
 {
   flake-minimal = {
-    path = ./minimal-flake;
+    path = ./flake-minimal;
     description = "A minimal nix flake template";
+  };
+
+  flake-utils = {
+    path = ./flake-utils;
+    description = "A nix flake template with flake-utils";
   };
 }
---
+++ nix/repo/templates/flake-utils/flake.nix
@@ -0,0 +1,145 @@
+{
+  description = ''
+    TODO DESCRIBE PROJECT HERE
+  '';
+
+  nixConfig = {
+    experimental-features = [
+      "flakes"
+      "nix-command"
+    ];
+
+    extra-substituters = [
+      # Add extra substituters here.
+    ];
+
+    extra-trusted-public-keys = [
+      # Add the public key of extra substituters here.
+    ];
+  };
+
+  # Nix packages
+  inputs = {
+    nixpkgs = {
+      follows = "nixpkgs-unstable";
+    };
+    nixpkgs-stable = {
+      url = "github:NixOS/nixpkgs/nixos-23.11";
+    };
+    nixpkgs-unstable = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+  };
+
+  # Nix libraries
+  inputs = {
+    devshell = {
+      url = "github:numtide/devshell/main";
+      inputs = {
+        flake-utils = {
+          follows = "flake-utils";
+        };
+        nixpkgs = {
+          follows = "nixpkgs";
+        };
+      };
+    };
+    flake-compat = {
+      url = "github:edolstra/flake-compat/master";
+      flake = false;
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils/main";
+      inputs = {
+        systems = {
+          follows = "nix-systems";
+        };
+      };
+    };
+    nix-systems = {
+      url = "github:nix-systems/default/main";
+    };
+    treefmt = {
+      url = "github:numtide/treefmt-nix/main";
+      inputs = {
+        nixpkgs = {
+          follows = "nixpkgs";
+        };
+      };
+    };
+  };
+
+  outputs =
+    {
+      devshell,
+      flake-utils,
+      nix-systems,
+      nixpkgs,
+      self,
+      treefmt,
+      ...
+    }@inputs:
+    let
+      inherit (nixpkgs.lib) genAttrs;
+
+      lib = nixpkgs.lib // builtins;
+
+      systems = import nix-systems;
+    in
+    flake-utils.lib.eachSystem systems (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+
+          overlays = [ devshell.overlays.default ];
+        };
+
+        treefmt' = treefmt.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+
+          programs = {
+            nixfmt = {
+              enable = true;
+              package = pkgs.nixfmt-rfc-style;
+            };
+          };
+        };
+      in
+      {
+        checks = {
+          formatting = treefmt'.config.build.check self;
+        };
+
+        devShells = {
+          default = pkgs.devshell.mkShell {
+            commands = [
+              # Add commands here.
+            ];
+
+            packages = [
+              # Add packages here.
+            ];
+          };
+        };
+
+        formatter = treefmt'.config.build.wrapper;
+
+        packages = {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "PROJECT";
+            src = ../.;
+            version = self.shortRev or "0000000";
+
+            nativeBuildInputs = [
+              # Add build dependencies here.
+            ];
+
+            buildPhase = ''
+              # Add build script here.
+            '';
+          };
+        };
+      }
+    );
+}
