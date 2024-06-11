e85f00a20791f59fd77d351fbc2f38e6aef84308
Burgess Chang <bsc@brsvh.org>
2024-06-11 09:58:13 +0800

    feat: let docker respect XDG Base Directory
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- nix/soil/nixosProfiles/docker/default.nix
+++ nix/soil/nixosProfiles/docker/default.nix
@@ -1,4 +1,13 @@
+{ cell, ... }:
 {
+  imports = [ cell.nixosProfiles.xdg ];
+
+  environment = {
+    sessionVariables = {
+      DOCKER_CONFIG = "$XDG_CONFIG_HOME/docker";
+    };
+  };
+
   virtualisation = {
     docker = {
       enable = true;
