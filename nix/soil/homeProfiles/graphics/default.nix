92303cc2917acea47c3f10d1ea939a001e7c2a71
Burgess Chang <bsc@brsvh.org>
2024-06-26 09:28:20 +0800

    chore: revert "fix: skip package which build failed"
    This reverts commit 0e29e86b63e4eecb16236d68812ae3fc577fcb1c.

--- nix/soil/homeProfiles/graphics/default.nix
+++ nix/soil/homeProfiles/graphics/default.nix
@@ -2,8 +2,7 @@
 {
   home = {
     packages = with pkgs; [
-      # REVIEW uncomment when it is fixed
-      # gimp-with-plugins
+      gimp-with-plugins
       graphviz
       imagemagick
       inkscape-with-extensions
