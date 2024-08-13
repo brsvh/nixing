44ef81bc0c925f05cb18a18480db3096effa0f7f
Burgess Chang <bsc@brsvh.org>
2024-08-13 16:58:11 +0800

    feat: kick chromium and edge
    Signed-off-by: Burgess Chang <bsc@brsvh.org>

--- nix/soil/homeProfiles/browser/default.nix
+++ nix/soil/homeProfiles/browser/default.nix
@@ -1,20 +1,6 @@
 { config, pkgs, ... }:
 {
-  home = {
-    packages = with pkgs; [ microsoft-edge ];
-
-    sessionVariables = {
-      GOOGLE_DEFAULT_CLIENT_ID = "699382231816-ubaprol9esc9qpeahi2o31iftf2tn9pv.apps.googleusercontent.com";
-      GOOGLE_DEFAULT_CLIENT_SECRET = "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";
-    };
-  };
-
   programs = {
-    chromium = {
-      enable = true;
-      commandLineArgs = [ ];
-    };
-
     firefox = {
       enable = true;
 
